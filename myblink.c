022b92e43f1807902221199912791d682b3acd00
Roman <roman.kalendariow@yandex.ru>
2024-10-02 19:29:26 +0300

    Repository created

---
+++ myblink.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wiringPi.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+
+#define GPIO_PIN 17
+
+int period = 2000;
+int pON = 50;
+int state = 1;
+
+void timeprint(char *s)
+{
+    time_t t = time(NULL);
+    struct tm *currentTime = localtime(&t);
+    
+    printf("[%02d:%02d:%02d] %s", currentTime->tm_hour, currentTime->tm_min, currentTime->tm_sec, s);
+}
+
+void* gpioHandler(void*) {
+    while (state) {
+        timeprint("led turned on >>>\n");
+        digitalWrite(GPIO_PIN, HIGH);
+        delay((int)(period / (100.0 / pON)));
+        
+        timeprint("led turned off <<<\n");
+        digitalWrite(GPIO_PIN, LOW);
+        delay(period - (int)(period / (100.0 / pON)));
+    }
+    return NULL;
+}
+
+int main() {
+    char input[100];
+
+    wiringPiSetupGpio();
+    pinMode(GPIO_PIN, OUTPUT);
+
+    pthread_t thread;
+    
+    if (pthread_create(&thread, NULL, gpioHandler, NULL)) {
+        fprintf(stderr, "pthread_create");
+        return -1;
+    }
+    
+    printf("use \"start\" or \"stop\" || use Ctrl+C for exit...\n");
+    
+    while (1) {
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
+        if (strcmp(input, "stop") == 0 && state)
+        {
+            state = 0;
+            pthread_join(thread, NULL);
+            digitalWrite(GPIO_PIN, LOW);
+            printf("led state = %s || input new period, pON:\n", state ? "on" : "off");
+            scanf("%d %d", &period, &pON);
+        }
+        else if (strcmp(input, "start") == 0 && !state)
+        {
+            state = 1;
+            if (pthread_create(&thread, NULL, gpioHandler, NULL)) {
+                fprintf(stderr, "pthread_create");
+                return -1;
+            }
+            printf("led state = %s || period = %d || pON = %d%%\n", state ? "on" : "off", period, pON);
+        }
+    }
+    
+    pthread_join(thread, NULL);
+    return 0;
+}
